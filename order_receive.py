bc9fa40752e5afc9e2d3640f46668cfe94c71789
obugrimov <obugrimov@avito.ru>
2016-06-25 17:51:56 +0300

    gotovo che

--- order_receive.py
+++ order_receive.py
@@ -1,18 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2009 Facebook
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
 
 import logging
 
@@ -38,10 +23,6 @@ class MessageBuffer(object):
         self.cache_size = 200
 
     def wait_for_messages(self, cursor=None):
-        # Construct a Future to return to our caller.  This allows
-        # wait_for_messages to be yielded from a coroutine even though
-        # it is not a coroutine itself.  We will set the result of the
-        # Future when results are available.
         result_future = Future()
         if cursor:
             new_count = 0
@@ -130,8 +111,6 @@ class CameOrderHandler(ApiHandler):
             "message": q_text,
             "created": datetime.datetime.now().strftime('%m/%d/%Y %H:%M:%S'),
         }
-        # to_basestring is necessary for Python 3's json encoder,
-        # which doesn't accept byte strings.
         message["html"] = tornado.escape.to_basestring(
             self.render_string("message.html", message=message))
         if self.get_argument("next", None):
